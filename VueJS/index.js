bb180f2a2db935e353cf36a5bc305c5e35174603
m-ripper <max@jovo.tech>
2019-10-18 18:01:28 +0200

    :sparkles: Implemented code for exercise #1

--- VueJS/index.js
+++ VueJS/index.js
@@ -1,3 +1,4 @@
+import AddButton from './components/AddButton.js';
 import List from './components/List.js'
 import ListItem from './components/ListItem.js'
 
@@ -9,5 +10,8 @@ new Vue({
       { id: '2', message: 'Bar', },
       { id: '3', message: 'Baz', }
     ]
-  }
+  },
+  template: `<div>` +
+      `<h1>ToDo List</h1><list v-model="todos"></list>` +
+      `</div>`
 })
