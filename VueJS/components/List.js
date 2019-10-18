522198cc04eb45f4e23be196c0156250400cd475
m-ripper <max@jovo.tech>
2019-10-18 18:07:20 +0200

    :sparkles: Implemented code for excercise #1

--- VueJS/components/List.js
+++ VueJS/components/List.js
@@ -1,1 +1,23 @@
-export default Vue.component('list', {})
+export default Vue.component('list', {
+    props: ['value'],
+    methods: {
+        handleDelete(index) {
+            this.value.splice(index, 1);
+        },
+        handleInput(newText, index) {
+            const newItem = {...this.value[index], message: newText};
+            this.value.splice(index, 1, newItem);
+        },
+        handleAdd(text) {
+            const newId = this.value.length + 1;
+            this.value.push({
+                id: newId,
+                message: text,
+            });
+        },
+    },
+    template: `<div class="list">` +
+        `<add-button @submit="handleAdd">Add ToDo</add-button>` +
+        `<list-item v-for="(item, index) in value" :key="'list-item-' + item.id" :item="item" @delete="handleDelete(index)" @input="handleInput($event, index)"></list-item>` +
+        `</div>`,
+});
--- VueJS/components/ListItem.js
+++ VueJS/components/ListItem.js
@@ -1,1 +1,40 @@
-export default Vue.component('list-item', {})
+export default Vue.component('list-item', {
+    props: ['item'],
+    data() {
+        return {
+            isEditing: false,
+            editText: '',
+        };
+    },
+    methods: {
+        handleEdit() {
+            this.isEditing = true;
+            this.editText = this.item.message;
+        },
+        handleCancel() {
+            this.isEditing = false;
+        },
+        handleSave() {
+            this.isEditing = false;
+            this.$emit('input', this.editText);
+        },
+        handleDelete() {
+            this.$emit('delete');
+        },
+    },
+    computed: {},
+    template: `<div class="list-item">{{item.id}}. ` +
+        `<template v-if="isEditing">` +
+        `<input v-model="editText" />` +
+        `</template>` +
+        `<span v-else>{{item.message}}</span>` +
+        `<div class="list-item-buttons">` +
+        `<template v-if="isEditing">` +
+        `<button @click="handleSave">Save</button>` +
+        `<button @click="handleCancel">Cancel</button>` +
+        `</template>` +
+        `<button v-else @click="handleEdit">Edit</button>` +
+        `<button @click="handleDelete">Delete</button>` +
+        `</div>` +
+        `</div>`,
+});
