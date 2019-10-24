7f7a4c256c69afa4f63de6c7c22a2e8b5ca265a4
Juliane Fink <jule-fink@gmx.de>
2019-10-24 11:25:15 +0200

    add vue-bootstrap

--- VueJS/ToDoApp/src/main.js
+++ VueJS/ToDoApp/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+Vue.use(BootstrapVue)
