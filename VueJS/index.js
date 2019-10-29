525627f954e9fba6b163328eb60de32cc3848803
Sopranoe <s0544642@htw-berlin.de>
2019-10-29 23:35:55 +0100

    Solution to exercise 1

--- VueJS/index.js
+++ VueJS/index.js
@@ -1,13 +1,69 @@
-import List from './components/List.js'
-import ListItem from './components/ListItem.js'
-
-new Vue({
-  el: '#app',
-  data: {
-    todos: [
-      { id: '1', message: 'Foo', },
-      { id: '2', message: 'Bar', },
-      { id: '3', message: 'Baz', }
-    ]
+Vue.component('list-item', {
+  props: ['todo'],
+  template: `
+      <div v-if="edit">
+        <li>
+          <input id="input" v-model="newTodo">
+          <button v-on:click="saveTodo">Save</button>
+          <button v-on:click="cancelTodo">Cancel</button>
+          <button v-on:click="$emit('deleteTodo')">Delete</button>
+        </li>
+      </div>
+      <div v-else>
+        <li>
+        {{ todo.message }}
+        <button v-on:click="editTodo">Edit</button>
+        <button v-on:click="$emit('deleteTodo')">Delete</button>
+        </li>
+      </div>
+  `,
+  data: function(){
+    return{
+      edit: false,
+      newTodo: ''
+    }
+  },
+  methods: {
+    editTodo: function(){
+      this.edit = true
+    },
+    cancelTodo: function(){
+      this.edit = false
+    },
+    saveTodo: function(){
+      this.todo.message = document.getElementById("input").value;
+      this.edit = false
+    }
   }
 })
+
+Vue.component('list', {
+  template: `
+    <ol>
+      <list-item
+        v-for="item in todos"
+        v-bind:todo="item"
+        v-bind:key="item.id"
+        v-on:deleteTodo="deleteTodo(id)">
+      </list-item>
+  </ol>
+  `,
+  data: function() {
+    return{
+      todos: [
+        { id: '1', message: 'Clean', },
+        { id: '2', message: 'Cook', },
+        { id: '3', message: 'Study', },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo: function(id){
+      this.todos.splice(id,1)
+    }
+  }
+})
+
+var app = new Vue({
+  el: '#app'
+});
