18481798af47b6675d597f8cffa8b6d7cb17df99
menno4000 <maxluedemann@t-online.de>
2019-11-10 16:54:35 +0100

    added empty app test spec added add entry event emission test

---
+++ vuejs3/test/App.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils';
+import app from '../src/App.vue';
+
+describe('App.vue', () => {
+  const wrapper = mount(app);
+  it('renders the todo app', () =>{
+    expect(wrapper.contains('div')).toBe(true);
+  });
+});
--- vuejs3/test/list.spec.js
+++ vuejs3/test/list.spec.js
@@ -2,12 +2,20 @@ import { mount } from '@vue/test-utils';
 import list from '../src/components/list.vue';
 
 describe('list.vue', () => {
-  it('renders a list item', () =>{
-    const items = [{text: '1'}, {text: '2'}, {text:'3'}]
-    const wrapper = mount(list, {
-      propsData: { items: items }
-    });
+  const items = [{text: '1'}, {text: '2'}, {text:'3'}]
+  const wrapper = mount(list, {
+    propsData: { items: items }
+  });
+  it('renders a list', () =>{
     expect(wrapper.contains('div')).toBe(true);
     expect(wrapper.findAll('p')).toHaveLength(items.length)
-  })
+  });
+  const buttons = wrapper.findAll('button');
+  const addButton = buttons.at((buttons.length-1));
+  describe('when the add entry button is pressed', () => {
+    addButton.trigger('click');
+    it('emits an addEntry event', () => {
+      expect(wrapper.emitted('addEntry'));
+    });
+  });
 })
