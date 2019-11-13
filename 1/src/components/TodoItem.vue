d1cd3e68342f48c9b75c32773ccac0fb8817c7e4
Johann R <johann.reuber@outlook.de>
2019-11-13 23:31:51 +0100

    WIP - Implemented some test cases, still need work to do

--- 1/src/components/TodoItem.vue
+++ 1/src/components/TodoItem.vue
@@ -29,7 +29,7 @@
         ,
         methods: {
             markComplete() {
-                this.todo.completed = !this.todo.completed
+                this.completed = !this.completed
             },
             editTodo(id, newMessage) {
                 this.$emit('edit-todo', id, newMessage)
--- 1/test/AddTodo.spec.js
+++ 1/test/AddTodo.spec.js
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 import AddTodo from '../src/components/AddTodo.vue'
 
 describe('AddTodo', () => {
-test('is a Vue instance', () => {
-const wrapper = mount(AddTodo)
-expect(wrapper.isVueInstance()).toBeTruthy()
-})
+    test('is a Vue instance', () => {
+        const wrapper = mount(AddTodo)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
 })
