4e98aad46b92a65f22b6a37db6ad3a6ad9d1da72
Christian Hesels <christianhesels@gmail.com>
2019-12-02 14:54:26 +0100

    Added test for creating Todo Item

--- 3/server/src/todos.spec.js
+++ 3/server/src/todos.spec.js
@@ -1,4 +1,33 @@
-it.todo("Create Todo")
-it.todo("Finish Todo")
-it.todo("Delete Todo")
-it.todo("Get Todos")
+const {ApolloServer, gql} = require('apollo-server-express');
+const {createTestClient} = require('apollo-server-testing');
+const typeDefs = require('../schema.js');
+const resolvers = require('../resolvers.js');
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+});
+
+
+const { query, mutate } = createTestClient(server);
+
+const CREATE_TODO = gql`
+       	mutation CreateTodo{
+               	addTodo(message: "Tests implementieren") {
+			id,
+                       	message,
+			completed
+               	}
+       	}
+`;
+	
+describe('Create Todo Item', () => {
+	it("Creates a new Todo", async () => {
+		const todo = await mutate({mutation: CREATE_TODO})
+		expect(todo.data).toMatchObject(
+			{ "addTodo": {"id": "3", "message": "Tests implementieren", "completed": false }}
+		)
+	})
+});
+
+
