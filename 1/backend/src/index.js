438c72de943df330d4a5f2cec7664f0852356b77
Hesels Christian <s0553310@htw-berlin.de>
2019-12-05 10:31:07 +0100

    Added driver to context and fixed JWT error with try and catch

--- 1/backend/src/index.js
+++ 1/backend/src/index.js
@@ -1,3 +1,4 @@
+const neo4j = require('neo4j-driver');
 const { ApolloServer } = require('apollo-server');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
@@ -5,15 +6,37 @@ const fs = require('fs');
 var SECRET_KEY = fs.readFileSync('./src/key/secret.key', 'utf8');
 
 const typeDefs = require('./schema');
-const resolvers = require('./resolvers')
+const resolvers = require('./resolvers');
+
+let driver;
+const uri = "bolt://127.0.0.1:7687";
+const username = "neo4j";
+const password = "neo4j";
+
+if (!driver) {
+  driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
+}
+
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req}) => {
-    const token = req.headers.authorization.replace('Bearer ', '')
+    let token = req.headers.authorization
     if (token){
-      return {token: jwt.verify(token, SECRET_KEY)}
+      token = token.replace('Bearer ', '')
+      try {
+        token = {token: jwt.verify(token, SECRET_KEY)}
+      }
+      catch(err) {
+        token = null
+      }
+    } else {
+      token = null
+    }
+    return {
+      driver,
+      token
     }
   }
 });
