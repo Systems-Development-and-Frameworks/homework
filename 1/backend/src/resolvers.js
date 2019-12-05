e7701fc45bc6f7c4fc26dc75533ea215ca64a451
Hesels Christian <s0553310@htw-berlin.de>
2019-12-05 20:43:13 +0100

    FIxed adding/deleting/finsihing todos
    	- Added users to Neo4j
    	- data.js is no longer in use

--- 1/backend/src/resolvers.js
+++ 1/backend/src/resolvers.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const { uuid } = require('uuidv4');
+const {uuid} = require('uuidv4');
 
-let data;
+let data, user;
 
 let SECRET_KEY = fs.readFileSync('./src/key/secret.key', 'utf8');
 
@@ -10,23 +10,22 @@ const resolvers = {
     Query: {
         todos: async (parent, args, context) => {
             // JSON.parse(JSON.stringify(data.todos))
-            const { driver } = context
+            const {driver} = context
             const getTodoCypher = `
-                    MATCH (todo)
+                    MATCH (todo:Todo)
                     RETURN todo.id, todo.message, todo.completed
                 `
 
             const session = driver.session()
             try {
-                data =  await session.run(getTodoCypher)
+                data = await session.run(getTodoCypher)
                 const todos = await data.records.map(record => ({
-                        id: record.get('todo.id'),
-                        message: record.get('todo.message'),
-                        completed: record.get('todo.completed')
+                    id: record.get('todo.id'),
+                    message: record.get('todo.message'),
+                    completed: record.get('todo.completed')
 
                 }))
                 json_data = JSON.parse(JSON.stringify(todos))
-                console.log(json_data)
                 return json_data
             } finally {
                 await session.close()
@@ -35,8 +34,8 @@ const resolvers = {
     },
     Mutation: {
         addTodo: async (parent, args, context) => {
-            if(args.message != "" && args.message != null && context.token) {
-                const { driver } = context
+            if (args.message != "" && args.message != null && context.token) {
+                const {driver} = context
                 const createTodoCypher = `
                     CREATE (todo:Todo {params})
                     RETURN todo
@@ -52,14 +51,13 @@ const resolvers = {
                 } finally {
                     await session.close()
                 }
-                data.todos.push(params);
                 return params;
             }
             return;
         },
         deleteTodo: async (parent, args, context) => {
-            if (args.id != null && context.token){
-                const { driver } = context
+            if (args.id != null && context.token) {
+                const {driver} = context
                 const deleteTodoCypher = `
                     MATCH (todo:Todo {id: $id})
                     DELETE todo
@@ -67,59 +65,93 @@ const resolvers = {
                 const session = driver.session()
                 try {
                     await session.run(deleteTodoCypher, {id: args.id})
+                    return true
                 } finally {
                     await session.close()
-                }
-                let index = data.todos.findIndex(td => td.id === args.id);
-                if (index > 0){
-                    data.todos.splice( index, 1)
-                    return true
                 }
                 return false
             }
         },
         editTodo: async (parent, args, context) => {
-            if (args.id != null && args.message != null && context.token){
-                const { driver } = context
-                const deleteTodoCypher = `
+            if (args.id != null && args.message != null && context.token) {
+                const {driver} = context
+                const editTodoCypher = `
                     MATCH (todo:Todo {id: $id})
                     SET todo.message = $message
+                    RETURN todo.id, todo.message, todo.completed
                 `
                 const session = driver.session()
                 try {
-                    await session.run(deleteTodoCypher, {id: args.id, message: args.message})
+                    const _response = await session.run(editTodoCypher, {id: args.id, message: args.message})
+                    const [response] = await _response.records.map(record => ({
+                        id: record.get('todo.id'),
+                        message: record.get('todo.message'),
+                        completed: record.get('todo.completed')
+                    }))
+                    if (response) {
+                        return response
+
+                    }
+                return null
+                } finally {
+                    await session.close()
+                }
+            }
+
+        },
+        finishTodo: async (parent, args, context) => {
+            if (args.id != null && context.token) {
+                const {driver} = context
+                const finishTodoCypher = `
+                MATCH (todo:Todo{id: $id})
+                SET todo.completed = True
+                RETURN todo.id, todo.message, todo.completed
+                `
+                const session = driver.session()
+                try {
+                    const _response = await session.run(finishTodoCypher, {id: args.id})
+                    const [response] = await _response.records.map(record => ({
+                        id: record.get('todo.id'),
+                        message: record.get('todo.message'),
+                        completed: record.get('todo.completed')
+                    }))
+                    if (response) {
+                        return response
+
+                    }
+                    return null
+                } finally {
+                    await session.close()
+                }
+            }
+        },
+        login: async (parent, args, context) => {
+            if (args.usr != null && args.pwd != null) {
+                let user
+                const {driver} = context
+                const getUserCypher = `
+                MATCH (user:User {login: $login, password: $password})
+                RETURN user.login, user.password
+                `
+                const session = driver.session()
+                try {
+                    user = await session.run(getUserCypher, {login: args.usr, password: args.pwd})
+
                 } finally {
                     await session.close()
                 }
 
-                tod = data.todos.find(td => td.id === args.id)
-                if (tod){
-                    tod.message = args.message;
-                    return tod;
-                }
-                return
-            }
+                const [currentUsr] = await user.records.map(record => ({
+                    login: record.get('user.login'),
+                    password: record.get('user.password')
 
-        },
-        finishTodo: (parent, args, context) => {
-            if (args.id != null && context.token){
-                let tod = data.todos.find(td => td.id === parseInt(args.id));
-                if (tod){
-                    //Reverse status of item
-                    tod.completed = !tod.completed;
-                    return tod
-                }
-                return
-            }
-        },
-        login: (parent, args) => {
-            if (args.usr != null && args.pwd != null){
-                let currentUsr = data.user.find(usr => usr.login === args.usr)
-                if (currentUsr != null && currentUsr.password === args.pwd){
+                }))
+
+                if (currentUsr.login === args.usr && currentUsr.password === args.pwd) {
                     const token = jwt.sign(
-                        { name: currentUsr.name, id: currentUsr.id },
+                        {name: currentUsr.name, id: currentUsr.id},
                         SECRET_KEY,
-                        { expiresIn: '1d' }
+                        {expiresIn: '1d'}
                     )
                     return token
                 }
@@ -128,17 +160,5 @@ const resolvers = {
         }
     }
 };
-
-function findNextId (){
-    let lastId = 0;
-    for (i = 0; i < data.todos.length; i++) {
-        if (data.todos[i].id > lastId) {
-            lastId = data.todos[i].id;
-        }
-    }
-    lastId += 1;
-
-    return lastId;
-}
 
 module.exports = resolvers;
