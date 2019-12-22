7d06b7c3949bb2e1a5b593e740f468650f34020f
Martin Neumann <martinneumann490@gmail.com>
2019-12-22 12:44:24 +0100

    start implementing seed method for dummy data for tests

--- backend/neo4j/seed.ts
+++ backend/neo4j/seed.ts
@@ -8,20 +8,53 @@ const password = "12345";
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 const session = driver.session();
 
-const userName = 'Alice';
-const resultPromise = session.run(
+/**
+ * Promises to write dummy data to database for testing.
+ */
+
+/**
+ * 1. Todos
+ */
+
+/**
+ * id: '83cab48e-5fb7-4ca0-b0de-3e6177e927ca',
+   description: 'Number one',
+   isDone: true,
+   assignedUser: users[0],
+   createdAt: new Date().toISOString(),
+ */
+
+const currentDate = Date.now();
+
+const todoPromise = session.run(
+    'CREATE (a:Todo { id: "83cab48e-5fb7-4ca0-b0de-3e6177e927ca", description: "Number one", isDone: "true") RETURN a',
+    { createdAt: currentDate }
+);
+
+
+/**
+ * 2. Users
+ */
+const userName = 'Bob';
+const userPromise = session.run(
     'CREATE (a:User {name: $name}) RETURN a',
     { name: userName }
 );
 
-resultPromise.then(result => {
+const promiseArray = [todoPromise, userPromise];
+
+
+/**
+ * Resolve all promises
+ */
+Promise.all(promiseArray).then(results => {
     session.close();
+    results.forEach(result => {
+        const singleRecord = result.records[0];
+        const node = singleRecord.get(0);
 
-    const singleRecord = result.records[0];
-    const node = singleRecord.get(0);
-
-    console.log(node.properties.name);
-
+        console.log(node.properties.name);
+    });
     // on application exit:
     driver.close();
 });
