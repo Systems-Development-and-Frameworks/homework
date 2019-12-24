624f20028fd5cd6b912202ef27901a3bf9739cec
Martin Neumann <martinneumann490@gmail.com>
2019-12-24 12:28:31 +0100

    finished seed method

--- backend/neo4j/seed.ts
+++ backend/neo4j/seed.ts
@@ -9,96 +9,104 @@ const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 const session = driver.session();
 
 /**
- * Promises to write dummy data to database for testing.
+ * Creates todos with the given parameters
+ * @param id The Todos unique Id
+ * @param description The Todos description of the activity
+ * @param isDone Whether the Todo is done (true) or not (false)
+ * @param createdAt When this Todo has been created
  */
 
-/**
- * 1. Todos
- */
-
-/**
- * id: '83cab48e-5fb7-4ca0-b0de-3e6177e927ca',
-   description: 'Number one',
-   isDone: true,
-   assignedUser: users[0],
-   createdAt: new Date().toISOString(),
- */
-
-const currentDate = Date.now();
-
-const todoPromise = session.run(
-    'CREATE (a:Todo {id: $id, description: $description, isDone: $isDone, createdAt: $createdAt}) RETURN a',
-    {
-        id: "83cab48e-5fb7-4ca0-b0de-3e6177e927ca",
-        description: "Build House",
-        isDone: "true",
-        createdAt: currentDate
-    },
-);
-
-todoPromise.then(result => {
-    console.log(`Ran todo promise`);
-
-
-    const userName = 'Bob';
-    const userPromise = session.run(
-        'CREATE (a:User {name: $name}) RETURN a',
-        { name: userName }
-    );
-    userPromise.then(result => {
-        console.log(`Ran user promise`);
-
-        const relationshipPromise = session.run(
-            'MATCH (a:User),(b:Todo)' +
-            'WHERE a.name = "Bob" AND b.id = "83cab48e-5fb7-4ca0-b0de-3e6177e927ca"' +
-            'CREATE (b)-[r:assignedTo]->(a)' +
-            'RETURN r'
-        );
-
-        relationshipPromise.then(result => {
-            console.log(`Ran relationship promise.`);
-
-        }).catch(message => {
-            console.log(message);
-            session.close();
+const createTodo = (id: string, description: string, isDone: boolean, createdAt: number) => {
+    return session.run('CREATE (a:Todo {id: $id, description: $description, isDone: $isDone, createdAt: $createdAt}) RETURN a',
+        {
+            "id": id,
+            "description": description,
+            "isDone": isDone,
+            "createdAt": createdAt
         });
-    }).catch(message => {
-        console.log(message);
-        session.close();
-    });
-}).catch(message => {
-    console.log(message);
-    session.close();
-});
-
-/*
-'CREATE (a:Todo) RETURN a',
-*/
-
-
-
-
-
-
-
-
-
+}
 
 /**
- * Resolve all promises
+ * Creates a user with the given parameters
+ * @param id The user's unique Id
+ * @param name The user's Name (non unique)
  */
-/*
-Promise.all(promiseArray).then(results => {
-    session.close();
-    results.forEach(result => {
-        const singleRecord = result.records[0];
-        const node = singleRecord.get(0);
+const createUser = (id: string, name: string) => {
+    return session.run(
+        'CREATE (a:User {id: $id, name: $name}) RETURN a',
+        {
+            id: id,
+            name: name
+        });
+}
 
-        console.log(node.properties.name);
-    });
-    // on application exit:
-    driver.close();
-}).catch(message => {
-    console.log(`Something went wrong: ${message}`);
+/**
+ * Creates a relationship from a todo to a user
+ * @todo relationship assignment is still hard coded
+ * @param userId the user in question
+ * @param todoId the todo in question
+ * @param relationship the relationship type
+ */
+
+const createRelation = (userId: string, todoId: string, relationship: string) => {
+    return session.run(
+        'MATCH (a:User),(b:Todo)' +
+        'WHERE a.id =' + '\"' + userId + '\"' + ' AND b.id = ' + '\"' + todoId + '\"' +
+        'CREATE(a) - [r: ' + relationship + '] -> (b)' +
+        'RETURN type(r)',
+    );
+}
+
+const deleteAllData = () => {
+    return session.run(
+        'MATCH (n) DETACH DELETE (n)'
+    )
+}
+/**
+ * Seed routine for testing
+ */
+
+deleteAllData().then(() => {
+
+    // 1. Creating Todos
+    createTodo("1", "Clean Room", false, Date.now()).then(() => {
+        createTodo("2", "Do Homework", false, Date.now()).then(() => {
+            createTodo("3", "Walk the Dog", true, Date.now()).then(() => {
+                createTodo("4", "Prepare Dinner", false, Date.now()).then(() => {
+                    createTodo("5", "Do Taxes", false, Date.now()).then(() => {
+                        console.log("All Todos done! Seeding Users...");
+
+                        // 2. Creating Users
+                        createUser("1", "Nelly").then(() => {
+                            createUser("2", "Fernando").then(() => {
+                                createUser("3", "Barack").then(() => {
+                                    createUser("4", "Alice").then(() => {
+                                        createUser("5", "Anderson").then(() => {
+                                            console.log("All Users done! Creating relationships...");
+
+                                            // 3. Creating relationships
+                                            createRelation("1", "3", "assignedTo").then(result => {
+                                                createRelation("1", "2", "assignedTo").then(() => {
+                                                    createRelation("2", "1", "assignedTo").then(() => {
+                                                        createRelation("3", "4", "assignedTo").then(() => {
+                                                            createRelation("4", "5", "assignedTo").then(() => {
+                                                                console.log("All seeding done!")
+                                                            })
+
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                });
+            })
+        })
+    })
+}).catch(error => {
+    console.log(`Something went wrong while seeding: ${JSON.stringify(error)} `);
 });
-*/
