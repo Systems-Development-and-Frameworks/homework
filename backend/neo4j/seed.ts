82b2de9dea5d1589eac711e5ecb56d9e1fa14cbb
Rico Stucke <rico_stucke@efa79.de>
2019-12-25 21:39:59 +0100

    seed funktion angepasst.

--- backend/neo4j/seed.ts
+++ backend/neo4j/seed.ts
@@ -1,9 +1,8 @@
-import neo4j from "neo4j-driver";
+import neo4j from 'neo4j-driver';
 
-
-const uri = "bolt://localhost:7687";
-const user = "neo4j";
-const password = "12345";
+const uri = 'bolt://localhost:7687';
+const user = 'neo4j';
+const password = '12345';
 
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 const session = driver.session();
@@ -19,12 +18,12 @@ const session = driver.session();
 const createTodo = (id: string, description: string, isDone: boolean, createdAt: number) => {
     return session.run('CREATE (a:Todo {id: $id, description: $description, isDone: $isDone, createdAt: $createdAt}) RETURN a',
         {
-            "id": id,
-            "description": description,
-            "isDone": isDone,
-            "createdAt": createdAt
+            id,
+            description,
+            isDone,
+            createdAt
         });
-}
+};
 
 /**
  * Creates a user with the given parameters
@@ -35,10 +34,10 @@ const createUser = (id: string, name: string) => {
     return session.run(
         'CREATE (a:User {id: $id, name: $name}) RETURN a',
         {
-            id: id,
-            name: name
+            id,
+            name
         });
-}
+};
 
 /**
  * Creates a relationship from a todo to a user
@@ -55,61 +54,61 @@ const createRelation = (userId: string, todoId: string, relationship: string) =>
         'CREATE(a) - [r: ' + relationship + '] -> (b)' +
         'RETURN type(r)',
     );
-}
+};
 
 const deleteAllData = () => {
     return session.run(
         'MATCH (n) DETACH DELETE (n)'
-    )
-}
+    );
+};
 /**
  * Seed routine for testing
  */
 
-export const seedData = () => {
+;(function () {
     return deleteAllData().then(() => {
 
         // 1. Creating Todos
-        createTodo("1", "Clean Room", false, Date.now()).then(() => {
-            createTodo("2", "Do Homework", false, Date.now()).then(() => {
-                createTodo("3", "Walk the Dog", true, Date.now()).then(() => {
-                    createTodo("4", "Prepare Dinner", false, Date.now()).then(() => {
-                        createTodo("5", "Do Taxes", false, Date.now()).then(() => {
-                            console.log("All Todos done! Seeding Users...");
+        createTodo('1', 'Clean Room', false, Date.now()).then(() => {
+            createTodo('2', 'Do Homework', false, Date.now()).then(() => {
+                createTodo('3', 'Walk the Dog', true, Date.now()).then(() => {
+                    createTodo('4', 'Prepare Dinner', false, Date.now()).then(() => {
+                        createTodo('5', 'Do Taxes', false, Date.now()).then(() => {
+                            console.log('All Todos done! Seeding Users...');
 
                             // 2. Creating Users
-                            createUser("1", "Nelly").then(() => {
-                                createUser("2", "Fernando").then(() => {
-                                    createUser("3", "Barack").then(() => {
-                                        createUser("4", "Alice").then(() => {
-                                            createUser("5", "Anderson").then(() => {
-                                                console.log("All Users done! Creating relationships...");
+                            createUser('1', 'Nelly').then(() => {
+                                createUser('2', 'Fernando').then(() => {
+                                    createUser('3', 'Barack').then(() => {
+                                        createUser('4', 'Alice').then(() => {
+                                            createUser('5', 'Anderson').then(() => {
+                                                console.log('All Users done! Creating relationships...');
 
                                                 // 3. Creating relationships
-                                                createRelation("1", "3", "assignedTo").then(result => {
-                                                    createRelation("1", "2", "assignedTo").then(() => {
-                                                        createRelation("2", "1", "assignedTo").then(() => {
-                                                            createRelation("3", "4", "assignedTo").then(() => {
-                                                                createRelation("4", "5", "assignedTo").then(() => {
+                                                createRelation('1', '3', 'assignedTo').then((result) => {
+                                                    createRelation('1', '2', 'assignedTo').then(() => {
+                                                        createRelation('2', '1', 'assignedTo').then(() => {
+                                                            createRelation('3', '4', 'assignedTo').then(() => {
+                                                                createRelation('4', '5', 'assignedTo').then(() => {
                                                                     session.close();
-                                                                    console.log("All seeding done!")
+                                                                    console.log('All seeding done!')
                                                                 })
 
                                                             })
                                                         })
                                                     })
-                                                })
-                                            })
-                                        })
-                                    })
-                                })
-                            })
-                        })
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
                     });
-                })
-            })
-        })
-    }).catch(error => {
+                });
+            });
+        });
+    }).catch((error) => {
         console.log(`Something went wrong while seeding: ${JSON.stringify(error)} `);
     });
-}
+})();
