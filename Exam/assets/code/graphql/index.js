8a21b6fa04e49764642b0a6743312c3adc12f4c8
roschaefer <git@roschaefer.de>
2020-01-03 17:44:04 +0100

    Write first draft of the exam
    My plan is to publish a couple of exam questions so our students can
    have a look and ask questions if some exercises descriptions are hard to
    understand. I would like to minimize the number of unpleasant surprises
    in the final exam.
    
    In the final version I will vary the content of some questions, remove questions
    and add a couple of new questions. The general structure should stay roughly the
    same.
    
    I've used
    https://de.overleaf.com/latex/templates/taif-university-exam-template/rpqzthxhtnsh
    and
    https://de.wikipedia.org/wiki/Datei:HTW_Berlin_logo.svg

---
+++ Exam/assets/code/graphql/index.js
@@ -0,0 +1,46 @@
+const { applyMiddleware } = require('graphql-middleware')
+const { ApolloServer, gql } = require('apollo-server')
+const { makeExecutableSchema } = require('graphql-tools')
+
+const typeDefs = `
+type Query {
+  hello(name: String): String
+  bye(name: String): String
+}
+`
+
+const logInput = async (resolve, root, args, context, info) => {
+  console.log(`1. logInput: ${JSON.stringify(args)}`)
+  const result = await resolve(root, args, context, info)
+  console.log(`5. logInput`)
+  return result
+}
+
+const logResult = async (resolve, root, args, context, info) => {
+  console.log(`2. logResult`)
+  const result = await resolve(root, args, context, info)
+  console.log(`4. logResult: ${JSON.stringify(result)}`)
+  return result
+}
+
+const middlewares = [logInput, logResult]
+
+const resolvers = {
+  Query: {
+    hello: (root, args, context, info) => {
+      console.log(`3. resolver: hello`)
+      return `Hello ${args.name ? args.name : 'world'}!`
+    },
+    bye: (root, args, context, info) => {
+      console.log(`3. resolver: bye`)
+      return `Bye ${args.name ? args.name : 'world'}!`
+    },
+  },
+}
+
+let schema = makeExecutableSchema({ typeDefs, resolvers })
+schema = applyMiddleware(schema, ...middlewares)
+const server = new ApolloServer({ schema });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
