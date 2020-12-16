c33bb15753ed74d2a3e3fdcd0d8323681fe90dd2
roschaefer <git@roschaefer.de>
2020-12-16 21:38:58 +0100

    docs: how to filter `passwordHashes` from client

--- demo/src/schema.js
+++ demo/src/schema.js
@@ -1,17 +1,21 @@
 import { stitchSchemas } from '@graphql-tools/stitch';
 import { applyMiddleware } from 'graphql-middleware';
+import { FilterObjectFields } from '@graphql-tools/wrap';
 import typeDefs from './typeDefs';
 import Resolvers from './resolvers';
 import permissions from './permissions';
 import GraphCmsSchema, { executor } from './graphCms/schema';
 
 export default async () => {
+  const transforms = [
+    new FilterObjectFields((typeName, fieldName) => (typeName !== 'Person' || fieldName !== 'passwordHash')),
+  ];
   const graphCmsSchema = await GraphCmsSchema();
   const resolvers = Resolvers([{ schema: graphCmsSchema, executor }]);
 
   let gatewaySchema = stitchSchemas({
     subschemas: [
-      graphCmsSchema,
+      { schema: graphCmsSchema, transforms },
     ],
     typeDefs,
     resolvers,
